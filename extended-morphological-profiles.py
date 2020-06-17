4554bcd1a38c1eb9e393206821f478dff2c72a9d
Andrey <andreybicalho@gmail.com>
2020-06-17 19:59:43 -0300

    fixed import; added dependency requirements

--- extended-morphological-profiles.py
+++ extended-morphological-profiles.py
@@ -241,7 +241,7 @@ for i in range(dim_x):
         cont += 1
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.75, random_state=0)
 
